d8916cbb216587352264e67cb1c8b3297e668093
gitc23 <16000325+gitc23@users.noreply.github.com>
2019-01-19 16:25:30 +0100

    add documentation, delete unused chunks

--- R/analyze_ueq.R
+++ R/analyze_ueq.R
@@ -1,24 +1,19 @@
+#' Summarize results for UEQ data
+#'
+#' Creates a list with the two dataframes UEQ items and UEQ scales.
+#'
+#' @param df dataframe with 26 columns in the order specified in the UEQ documentation
+#'
+#' @return list with two dataframes
+#'
+#' @examples
+#' analyze_ueq()
+#'
+#' @export
 analyze_ueq <- function(df) {
-
   # add_vars <- colnames(sauber)[27:length(colnames(sauber))]
-  
   item_results <- ueq_items(df)
-  
-  # wenn Namen gewünscht
   # item_results <- cbind(Item = data.frame(item_names_long), item_results)
-  
   scales_results <- ueq_scales(df)
-  
-  # scale_means <- sauber %>% 
-  #   mutate(Attraktivität = round(rowMeans(sauber[unlist(scales[1])]), digits = 2),
-  #          Durchschaubarkeit = round(rowMeans(sauber[unlist(scales[2])]), digits = 2),
-  #          Effizienz = round(rowMeans(sauber[unlist(scales[3])]), digits = 2),
-  #          Steuerbarkeit = round(rowMeans(sauber[unlist(scales[4])]), digits = 2),
-  #          Stimulation = round(rowMeans(sauber[unlist(scales[5])]), digits = 2),
-  #          Originalität = round(rowMeans(sauber[unlist(scales[6])]), digits = 2)) %>% 
-  #   select(Attraktivität, Durchschaubarkeit, Effizienz, Steuerbarkeit, Stimulation, Originalität, add_vars)
-  
   list("items" = item_results, "scales" = scales_results)
- 
-  #return(results_list)
 }
