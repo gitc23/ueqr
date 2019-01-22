7e19a7fcbbb721f8a9060d143db6ca5350eff7cd
gitc23 <16000325+gitc23@users.noreply.github.com>
2019-01-22 23:58:09 +0100

    add new function

--- R/analyze_ueq.R
+++ R/analyze_ueq.R
@@ -1,19 +1,32 @@
 #' Summarize results for UEQ data
 #'
-#' Creates a list with the two dataframes UEQ items and UEQ scales.
+#' update
 #'
 #' @param df dataframe with 26 columns in the order specified in the UEQ documentation
 #'
-#' @return list with two dataframes
-#'
-#' @examples
-#' analyze_ueq()
+#' @param df dataframe with 26 columns in the order specified in the UEQ documentation
+#' @param is.clean Boolean whether the passed dataframe is already cleaned
+#' @param ueq_range the range of columns where the 26 UEQ Items are to be found
+#' @param group_vars index of additional variables that should not be dropped
 #'
 #' @export
-analyze_ueq <- function(df) {
-  # add_vars <- colnames(sauber)[27:length(colnames(sauber))]
-  item_results <- ueq_items(df)
-  # item_results <- cbind(Item = data.frame(item_names_long), item_results)
-  scales_results <- ueq_scales(df)
-  list("items" = item_results, "scales" = scales_results)
+analyze_ueq <- function(df, is.clean = FALSE, ueq_range = c(1:26), group_var = 27) {
+
+  scales <- list(attractiveness = c(1,12,14,16,24,25),
+                 perspicuity = c(2,4,13,21),
+                 efficiency = c(9,20,22,23),
+                 dependability = c(8,11,17,19),
+                 stimulation = c(5,6,7,18),
+                 novelty = c(3,10,15,26))
+
+  if (!is.clean) {
+    scale_means <- ueq_case_scale_means(df, is.clean = FALSE, ueq_range = ueq_range, add_vars = group_var)
+  } else {
+    scale_means <- ueq_case_scale_means(df, is.clean = TRUE, ueq_range = ueq_range, add_vars = group_var)
+  }
+
+  ggpubr::compare_means(c(attractiveness, perspicuity, efficiency, dependability, stimulation, novelty)
+                        ~ Gruppe1, data = scale_means)
+  ### Gruppe anpassen
+
 }
