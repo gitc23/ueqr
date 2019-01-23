eaf1087f36a4de1e5ad10f54d6a67e603a669767
gitc23 <16000325+gitc23@users.noreply.github.com>
2019-01-23 11:43:24 +0100

    update function params, add universal group, as function only compares per group

--- R/analyze_ueq.R
+++ R/analyze_ueq.R
@@ -4,13 +4,15 @@
 #'
 #' @param df dataframe with 26 columns in the order specified in the UEQ documentation
 #'
-#' @param df dataframe with 26 columns in the order specified in the UEQ documentation
 #' @param is.clean Boolean whether the passed dataframe is already cleaned
 #' @param ueq_range the range of columns where the 26 UEQ Items are to be found
-#' @param group_vars index of additional variables that should not be dropped
+#' @param add_vars index of additional variables that should not be dropped
+#' @param group_var index of additional variables that should not be dropped
+#'
+#' @import dplyr
 #'
 #' @export
-analyze_ueq <- function(df, is.clean = FALSE, ueq_range = c(1:26), group_var = 27) {
+analyze_ueq <- function(df, is.clean = FALSE, ueq_range = c(1:26), add_vars = NULL, group_var = 27) {
 
   scales <- list(attractiveness = c(1,12,14,16,24,25),
                  perspicuity = c(2,4,13,21),
@@ -21,12 +23,16 @@ analyze_ueq <- function(df, is.clean = FALSE, ueq_range = c(1:26), group_var = 2
 
   if (!is.clean) {
     scale_means <- ueq_case_scale_means(df, is.clean = FALSE, ueq_range = ueq_range, add_vars = group_var)
+    colnames(scale_means)[7] <- c("group")
   } else {
     scale_means <- ueq_case_scale_means(df, is.clean = TRUE, ueq_range = ueq_range, add_vars = group_var)
+    colnames(scale_means)[7] <- c("group")
   }
 
   ggpubr::compare_means(c(attractiveness, perspicuity, efficiency, dependability, stimulation, novelty)
-                        ~ Gruppe1, data = scale_means)
-  ### Gruppe anpassen
+                        ~ group, data = scale_means)
 
 }
+
+
+
