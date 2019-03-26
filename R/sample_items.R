c2dc17b7b06d8f661a1430ec6bd93335b0654a52
gitc23 <16000325+gitc23@users.noreply.github.com>
2019-03-26 20:44:54 +0100

    added dataset

---
+++ R/sample_items.R
@@ -0,0 +1,15 @@
+#' UEQ sample data
+#'
+#' Data from https://www.ueq-online.org, with the 26 UEQ items on a scale from 1 to 7
+#'
+#' @docType data
+#'
+#' @usage data(grav)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://www.ueq-online.org}{UEQ Online}
+#'
+"sample_items"
